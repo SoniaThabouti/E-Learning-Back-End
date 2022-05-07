3e43c11b9404d9adcc1df1a9f63403bc67ae6c61
takwa <takwa.rouini@ensi-uma.tn>
2022-05-07 02:00:18 +0200

    deletetimetableclass

--- src/main/java/ElearningBack/repository/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package ElearningBack.repository;
-
-import ElearningBack.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface TestRepository extends JpaRepository<Test,Long> {
-}
